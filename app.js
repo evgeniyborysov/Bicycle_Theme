34700c92bbc2ee4458bfe9770745c267e75a125d
evgeniy.borysov <evgeniy.borysov@mail.com>
2021-06-01 14:22:16 +0300

    done

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ $('.my-slider').slick({
     slidesToScroll: 1,
     arrows: false,
     dots: true,
-    dotsClass: 'slick-dots'
+    dotsClass: 'slick-dots',
+    autoplay: true,
+    autoplaySpeed: 2000
 });
+
+$('#burger').on('click', function () {
+    $('nav').toggleClass('open');
+});
+
+
 
 
 console.log("1");
